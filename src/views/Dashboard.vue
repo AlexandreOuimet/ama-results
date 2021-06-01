ddc3ba887ecb0a54c35a946a186709e3bf69d4e0
Alexandre Ouimet <alexandreouimet38@gmail.com>
2021-06-01 15:02:14 -0400

    clean

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,48 @@
 <template>
-  <v-container fluid> </v-container>
+  <v-container> </v-container>
 </template>
 
 <script>
+import axios from "axios";
+import cheerio from "cheerio";
+
 export default {
   name: "Home",
   components: {},
+  methods: {
+    fetch() {
+      var url = "http://americanmotocrosslive.com/xml/mx/RaceResultsWeb.xml";
+
+      console.log("URL", url);
+
+      axios.get(url).then((response) => {
+        const xml = response.data;
+        const $ = cheerio.load(xml);
+
+        const elements = $("b");
+        var raceResults = [];
+
+        for (var element of elements) {
+          const rider = {
+            position: element.attribs["a"],
+            name: element.attribs["f"],
+            number: element.attribs["n"],
+            vehicle: element.attribs["v"],
+            location: element.attribs["t"],
+            class: element.attribs["c"],
+            nbLaps: element.attribs["l"],
+            lastLap: element.attribs["ll"],
+            bestLap: element.attribs["bl"],
+            diff: element.attribs["d"],
+            gap: element.attribs["g"],
+            status: element.attribs["s"],
+          };
+          raceResults.push(rider);
+        }
+        console.log(raceResults);
+      });
+    },
+  },
 };
 </script>
 
